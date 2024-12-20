772530e03208de7cd434e9859e3be0595b39fbf9
Kapoors-Hub <pranjil@mousencheese.design>
2024-12-20 19:31:40 +0500

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -17,7 +17,7 @@ const Home = () => {
     try {
       setError(null);
       const response = await axios.get(
-        `https://newsapi.org/v2/top-headlines?country=us&apiKey=${import.meta.env.VITE_NEWS_API_KEY}`
+        `https://newsapi.org/v2/top-headlines?country=us&apiKey=0086ff0595874eddbc143c86b81441e2`
       );
       setNews(response.data.articles);
     } catch (error) {
